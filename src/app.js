d533cacb058ba2a21345e0e25617c1bccdcc51fb
Adrian <adibu8@gmail.com>
2021-04-20 17:43:08 +0200

    Add errorHandler

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("./db/db-connection");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("Server is running on " + PORT);
