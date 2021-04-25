4bc6451a484c7d2c6453d448846cd595d0a360d6
Adrian <adibu8@gmail.com>
2021-04-25 20:29:56 +0200

    Create insurance raport route and controller method

--- src/controllers/offer.js
+++ src/controllers/offer.js
@@ -53,3 +53,24 @@ exports.getOffersByServicer = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getOffersRaportInsurance = async (req, res, next) => {
+  try {
+    const offers = await Offer.find(
+      { serviceType: "ubezpieczenie" },
+      { _id: 0, oneTimePrice: 1, insuranceCoverage: 1, OC: 1 }
+    )
+      .sort({ oneTimePrice: 1 })
+      .populate("servicer", { name: 1, _id: 0 })
+      .exec();
+    if (!offers) {
+      createError("Could not find offers", 404);
+    }
+    return res.send(offers);
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
--- src/routers/offer.js
+++ src/routers/offer.js
@@ -3,12 +3,14 @@ const {
   getOffers,
   getOffer,
   getOffersByServicer,
+  getOffersRaportInsurance,
 } = require("../controllers/offer");
 
 const router = express.Router();
 
 router.get("/offers", getOffers);
-router.get("/offers/:offerId", getOffer);
+router.get("/offers/id/:offerId", getOffer);
 router.get("/offers/servicer/:servicerId", getOffersByServicer);
+router.get("/offers/raport/insurance", getOffersRaportInsurance);
 
 module.exports = router;
