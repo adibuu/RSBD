40576ddb4c55a9f6f2d3420f0a7fb72f9aa289ff
Adrian <adibu8@gmail.com>
2021-04-27 15:38:50 +0200

    Comment AdminBro

--- src/app.js
+++ src/app.js
@@ -5,24 +5,24 @@ const errorHandler = require("./middleware/errorHandler");
 const servicerRouter = require("./routers/servicer");
 const offerRouter = require("./routers/offer");
 const notFoundRouter = require("./routers/404");
-const Servicer = require("./models/servicer");
-const Offer = require("./models/offer");
+// const Servicer = require("./models/servicer");
+// const Offer = require("./models/offer");
 
-const AdminBro = require("admin-bro");
-const AdminBroExpress = require("@admin-bro/express");
-const AdminBroMongoose = require("@admin-bro/mongoose");
+// const AdminBro = require("admin-bro");
+// const AdminBroExpress = require("@admin-bro/express");
+// const AdminBroMongoose = require("@admin-bro/mongoose");
 
-AdminBro.registerAdapter(AdminBroMongoose);
-const adminBro = new AdminBro({
-  resources: [Servicer, Offer],
-  rootPath: "/admin",
-});
-const router = AdminBroExpress.buildRouter(adminBro);
+// AdminBro.registerAdapter(AdminBroMongoose);
+// const adminBro = new AdminBro({
+//   resources: [Servicer, Offer],
+//   rootPath: "/admin",
+// });
+// const router = AdminBroExpress.buildRouter(adminBro);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(adminBro.options.rootPath, router);
+//app.use(adminBro.options.rootPath, router);
 app.use(cors());
 app.use(express.json());
 app.use(servicerRouter);
